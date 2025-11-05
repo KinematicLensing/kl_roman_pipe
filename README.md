7e5fc1aabb3ebc37ce124657e567a673965a7e6a
Spencer Everett <spencerweverett@gmail.com>
2025-11-04 17:49:51 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kl_roman_test
 Test repo to try out a new pipeline approach for Roman KL
+
+## Installation instructions
+
+1) Get `conda` (I recommend [miniforge](https://github.com/conda-forge/miniforge))
+2) Get `conda-lock` in your base env: `conda install conda-lock`
+3) `make install`
+4) `make test`
---
+++ install.sh
@@ -0,0 +1,49 @@
+# exit as soon as a command fails:
+set -e
+
+REPO_DIR="$(realpath "$(dirname "$0")")"
+CWD=$(pwd)
+
+ENVFILE="$(dirname "$0")"/environment.yaml
+ENVNAME="$(grep '^name:' "$ENVFILE" | cut -d' ' -f2)"
+
+echo ENVFILE="$ENVFILE"
+echo ENVNAME="$ENVNAME"
+
+# Always execute this script with bash, so that conda shell.hook works.
+# Relevant conda bug: https://github.com/conda/conda/issues/7980
+if test "$BASH_VERSION" = ""
+then
+    exec bash "$0" "$@"
+fi
+
+eval "$(conda shell.bash hook)"
+
+# check if the environment already exists
+if conda info --envs | grep -q "$ENVNAME"; then
+    echo "Environment '$ENVNAME' already exists. Removing it first..."
+    conda deactivate || true
+    conda env remove -n "$ENVNAME" --yes
+fi
+
+# install environment fresh
+echo "Installing '$ENVNAME' from reproducible conda-lock.yml..."
+conda run -n base conda-lock install --name "$ENVNAME" "$REPO_DIR/conda-lock.yml"
+
+# activate conda environment
+conda activate "$ENVNAME"
+
+echo "cd $REPO_DIR"
+cd $REPO_DIR
+
+echo "Pip installing kl_roman_test..."
+pip install --no-build-isolation --no-deps --editable "$REPO_DIR/."
+
+#echo "Pip installing my special repo..."
+#pip install --no-build-isolation --no-deps --editable "$PATH_TO_REPO/."
+
+echo "cd $CWD"
+cd "$CWD"
+
+echo "conda deactivate"
+conda deactivate
