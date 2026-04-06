934843dd9ebe67af35e62f2faf3a21057d8995d9
Yan Xiang Lai <ylai2@arizona.edu>
2026-04-06 11:23:44 -0700

    Updating the galaxy id of the galaxy in the new gold catalogue since they are different from the old catalogue. Also updating the matplotlib backend string to use double quotes for consistency.

--- tests/test_psf_tng.py
+++ tests/test_psf_tng.py
@@ -11,7 +11,7 @@ import pytest
 import numpy as np
 import matplotlib
 
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from pathlib import Path
 
@@ -35,7 +35,8 @@ def tng_generator():
     from kl_pipe.tng import TNG50MockData, TNGDataVectorGenerator
 
     tng_data = TNG50MockData()
-    galaxy = tng_data.get_galaxy(subhalo_id=8)
+    # galaxy = tng_data.get_galaxy(subhalo_id=8)
+    galaxy = tng_data.get_galaxy(index=0)
     return TNGDataVectorGenerator(galaxy)
 
 
@@ -44,7 +45,7 @@ def tng_config():
     """Render config for TNG tests."""
     from kl_pipe.tng.data_vectors import TNGRenderConfig
 
-    image_pars = ImagePars(shape=(64, 64), pixel_scale=0.11, indexing='xy')
+    image_pars = ImagePars(shape=(64, 64), pixel_scale=0.11, indexing="xy")
     return TNGRenderConfig(
         image_pars=image_pars,
         target_redshift=0.5,
@@ -92,22 +93,22 @@ def test_tng_intensity_with_psf(tng_generator, tng_config, tng_psf, output_dir):
     # diagnostic plot
     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
-    im0 = axes[0].imshow(intensity_no_psf, origin='lower', cmap='viridis')
-    axes[0].set_title('No PSF')
+    im0 = axes[0].imshow(intensity_no_psf, origin="lower", cmap="viridis")
+    axes[0].set_title("No PSF")
     plt.colorbar(im0, ax=axes[0])
 
-    im1 = axes[1].imshow(intensity_psf, origin='lower', cmap='viridis')
-    axes[1].set_title('With PSF')
+    im1 = axes[1].imshow(intensity_psf, origin="lower", cmap="viridis")
+    axes[1].set_title("With PSF")
     plt.colorbar(im1, ax=axes[1])
 
     diff = intensity_psf - intensity_no_psf
-    im2 = axes[2].imshow(diff, origin='lower', cmap='RdBu_r')
-    axes[2].set_title('Difference')
+    im2 = axes[2].imshow(diff, origin="lower", cmap="RdBu_r")
+    axes[2].set_title("Difference")
     plt.colorbar(im2, ax=axes[2])
 
-    fig.suptitle(f'TNG SubhaloID 8 Intensity (flux diff: {rel_flux_diff:.2e})')
+    fig.suptitle(f"TNG SubhaloID 8 Intensity (flux diff: {rel_flux_diff:.2e})")
     plt.tight_layout()
-    plt.savefig(output_dir / 'tng_intensity_psf_comparison.png', dpi=150)
+    plt.savefig(output_dir / "tng_intensity_psf_comparison.png", dpi=150)
     plt.close()
 
 
@@ -143,25 +144,25 @@ def test_tng_velocity_with_psf(tng_generator, tng_config, tng_psf, output_dir):
     vmax = max(np.percentile(velocity_no_psf, 98), np.percentile(velocity_psf, 98))
 
     im0 = axes[0].imshow(
-        velocity_no_psf, origin='lower', cmap='RdBu_r', vmin=vmin, vmax=vmax
+        velocity_no_psf, origin="lower", cmap="RdBu_r", vmin=vmin, vmax=vmax
     )
-    axes[0].set_title(f'No PSF (range={range_no_psf:.0f})')
+    axes[0].set_title(f"No PSF (range={range_no_psf:.0f})")
     plt.colorbar(im0, ax=axes[0])
 
     im1 = axes[1].imshow(
-        velocity_psf, origin='lower', cmap='RdBu_r', vmin=vmin, vmax=vmax
+        velocity_psf, origin="lower", cmap="RdBu_r", vmin=vmin, vmax=vmax
     )
-    axes[1].set_title(f'With PSF (range={range_psf:.0f})')
+    axes[1].set_title(f"With PSF (range={range_psf:.0f})")
     plt.colorbar(im1, ax=axes[1])
 
     diff = velocity_psf - velocity_no_psf
-    im2 = axes[2].imshow(diff, origin='lower', cmap='RdBu_r')
-    axes[2].set_title('Difference')
+    im2 = axes[2].imshow(diff, origin="lower", cmap="RdBu_r")
+    axes[2].set_title("Difference")
     plt.colorbar(im2, ax=axes[2])
 
-    fig.suptitle('TNG SubhaloID 8 Velocity')
+    fig.suptitle("TNG SubhaloID 8 Velocity")
     plt.tight_layout()
-    plt.savefig(output_dir / 'tng_velocity_psf_comparison.png', dpi=150)
+    plt.savefig(output_dir / "tng_velocity_psf_comparison.png", dpi=150)
     plt.close()
 
 
